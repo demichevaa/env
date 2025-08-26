82a8b2424ed9fb375a4642d644543ccf016f53dd
Alexander Demichev <demichevaa@yahoo.com>
2025-08-26 18:47:13 +0700

    Add: gnome setup.

---
+++ gnome/deploy.sh
@@ -0,0 +1,11 @@
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+gsettings set org.gnome.desktop.interface enable-hot-corners false
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.nautilus.preferences show-hidden-files true
+gsettings set org.gnome.nautilus.preferences always-use-location-entry true
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.desktop.interface clock-format '24h'
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['F1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['F2']"
+
